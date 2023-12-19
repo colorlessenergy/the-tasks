7acaf4eba298b353cab163e2cd3bc3c05739a294
colorlessenergy <brianmunozdev@gmail.com>
2023-12-19 08:38:26 -0800

    add HTML and CSS for add item button

--- app/page.js
+++ app/page.js
@@ -11,6 +11,12 @@ export default function Home() {
 
                 <div>10 tasks</div>
             </div>
+
+            <button className="add-item-button">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path d="M11 11V5H13V11H19V13H13V19H11V13H5V11H11Z"></path>
+                </svg>
+            </button>
         </div>
     );
 }
