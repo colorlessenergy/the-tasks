833e2ca27b469c5b263752b6f416f47ab535f35c
colorlessenergy <brianmunozdev@gmail.com>
2023-12-26 16:33:13 -0800

    add functionality to edit and delete a list

--- app/_components/Form.js
+++ app/_components/Form.js
@@ -1,6 +1,6 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-const Form = ({ setLists, toggleModal }) => {
+const Form = ({ setLists, toggleModal, isEditingList, setIsEditingList }) => {
     const [inputValue, setInputValue] = useState('');
     const handleChange = event => {
         setInputValue(event.currentTarget.value);
@@ -10,10 +10,19 @@ const Form = ({ setLists, toggleModal }) => {
         event.preventDefault();
 
         let lists = JSON.parse(localStorage.getItem('lists'));
-        lists.push({
-            name: inputValue,
-            tasks: []
-        });
+        if (isEditingList) {
+            lists[isEditingList.index] = {
+                name: inputValue,
+                tasks: isEditingList.tasks
+            };
+            setIsEditingList(false);
+        } else {
+            lists.push({
+                name: inputValue,
+                tasks: []
+            });
+        }
+
         localStorage.setItem('lists', JSON.stringify(lists));
         setLists(lists);
 
@@ -21,8 +30,25 @@ const Form = ({ setLists, toggleModal }) => {
         toggleModal();
     };
 
+    const handleDeleteItem = () => {
+        let lists = JSON.parse(localStorage.getItem('lists'));
+        lists.splice(isEditingList.index, 1);
+        localStorage.setItem('lists', JSON.stringify(lists));
+        setLists(lists);
+
+        setIsEditingList(false);
+        setInputValue('');
+        toggleModal();
+    };
+
+    useEffect(() => {
+        if (isEditingList.name) {
+            setInputValue(isEditingList.name);
+        }
+    }, [isEditingList]);
+
     return (
-        <form onSubmit={handleSubmit} className="add-item-form">
+        <form onSubmit={handleSubmit} className="item-form">
             <label htmlFor="add-list" className="mb-05">
                 list
             </label>
@@ -31,9 +57,23 @@ const Form = ({ setLists, toggleModal }) => {
                 value={inputValue}
                 onChange={handleChange}
                 id="add-list"
-                className="add-item-form-input"
+                className="item-form-input"
             />
-            <button className="add-item-form-button">add</button>
+
+            {isEditingList ? (
+                <div className="item-form-buttons">
+                    <button
+                        className="item-form-button bg-color-red"
+                        onClick={handleDeleteItem}
+                        type="button">
+                        delete
+                    </button>
+
+                    <button className="item-form-button">edit</button>
+                </div>
+            ) : (
+                <button className="item-form-button">add</button>
+            )}
         </form>
     );
 };
--- app/page.js
+++ app/page.js
@@ -18,6 +18,12 @@ export default function Home() {
         setLists(getLists());
     }, []);
 
+    const [isEditingList, setIsEditingList] = useState(false);
+    const handleEditList = list => {
+        setIsEditingList(list);
+        toggleModal();
+    };
+
     return (
         <div>
             {lists.map((list, index) => {
@@ -25,11 +31,18 @@ export default function Home() {
                     <div key={index} className="list mb-2">
                         <div className="list-header mb-1">
                             {list.name}
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 24 24">
-                                <path d="M4.5 10.5C3.675 10.5 3 11.175 3 12C3 12.825 3.675 13.5 4.5 13.5C5.325 13.5 6 12.825 6 12C6 11.175 5.325 10.5 4.5 10.5ZM19.5 10.5C18.675 10.5 18 11.175 18 12C18 12.825 18.675 13.5 19.5 13.5C20.325 13.5 21 12.825 21 12C21 11.175 20.325 10.5 19.5 10.5ZM12 10.5C11.175 10.5 10.5 11.175 10.5 12C10.5 12.825 11.175 13.5 12 13.5C12.825 13.5 13.5 12.825 13.5 12C13.5 11.175 12.825 10.5 12 10.5Z"></path>
-                            </svg>
+
+                            <button
+                                onClick={() =>
+                                    handleEditList({ ...list, index })
+                                }
+                                className="button-svg">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24">
+                                    <path d="M4.5 10.5C3.675 10.5 3 11.175 3 12C3 12.825 3.675 13.5 4.5 13.5C5.325 13.5 6 12.825 6 12C6 11.175 5.325 10.5 4.5 10.5ZM19.5 10.5C18.675 10.5 18 11.175 18 12C18 12.825 18.675 13.5 19.5 13.5C20.325 13.5 21 12.825 21 12C21 11.175 20.325 10.5 19.5 10.5ZM12 10.5C11.175 10.5 10.5 11.175 10.5 12C10.5 12.825 11.175 13.5 12 13.5C12.825 13.5 13.5 12.825 13.5 12C13.5 11.175 12.825 10.5 12 10.5Z"></path>
+                                </svg>
+                            </button>
                         </div>
 
                         <div>
@@ -47,7 +60,12 @@ export default function Home() {
             </button>
 
             <Modal isOpen={isModalOpen} toggleModal={toggleModal}>
-                <Form setLists={setLists} toggleModal={toggleModal} />
+                <Form
+                    setLists={setLists}
+                    toggleModal={toggleModal}
+                    isEditingList={isEditingList}
+                    setIsEditingList={setIsEditingList}
+                />
             </Modal>
         </div>
     );
