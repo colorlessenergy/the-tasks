dd32c9b5ff41c29b287ccf8bc705a3fcee5a877c
colorlessenergy <brianmunozdev@gmail.com>
2023-12-27 13:47:36 -0800

    add link to lists

--- app/list/[index]/page.js
+++ app/list/[index]/page.js
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import { usePathname } from 'next/navigation';
+import Link from 'next/link';
 
 import { getLists } from '@/app/_utilities/lists';
 
@@ -14,5 +15,15 @@ export default function List() {
         setList(getLists()[listIndex]);
     }, []);
 
-    return <h1>list</h1>;
+    return (
+        <div>
+            <div className="tasks-header">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path d="M10.8284 12.0007L15.7782 16.9504L14.364 18.3646L8 12.0007L14.364 5.63672L15.7782 7.05093L10.8284 12.0007Z"></path>
+                </svg>
+
+                <Link href="/">lists</Link>
+            </div>
+        </div>
+    );
 }
